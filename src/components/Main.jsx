0037431b7ae41db338700a102a1945475cf0b146
SeptianAdi08 <nugrohoseptian48@gmail.com>
2024-02-09 23:31:01 +0700

    change Requests.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import requests from '../Requests'
+import axios from 'axios'
+
 
 const Main = () => {
+    const [movies, setMovies] = useState([])
+
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+
+    useEffect(() => {
+      axios.get(requests.requestPopular).then((response)=>{
+        setMovies(response.data.results)        
+      })
+    },[])
+    console.log(movies)
+
+    
+
   return (
     <div>
       
